9cae5076834607d27a80280174e3180304847d52
P-Sudhakar <sumanpalani1703@gmail.com>
2024-08-13 11:25:22 +0530

    jenkins pipeline script added

---
+++ NetFlix_Clone.groovy
@@ -0,0 +1,43 @@
+pipeline {
+    agent any
+    stages {
+        stage('Checkout from Git') {
+            steps {
+                git 'https://github.com/P-Sudhakar/Netflix_clone'
+            }
+        }
+        stage ('Build a Docker File') {
+            steps {
+                script{
+                   withDockerRegistry(credentialsId: 'docker', toolName: 'docker'){   
+                       sh "docker build -t netflix ."
+                    }
+                }
+            }
+        } 
+        stage ('Tag A DockerImage') {
+            steps {
+                script{
+                   withDockerRegistry(credentialsId: 'docker', toolName: 'docker'){   
+                       sh "docker tag netflix sudhakarp1703/netflix:latest "
+                   }      
+                }               
+            }
+        }   
+         stage ('Push DockerImage to the DockerHub') {
+            steps {
+                script{
+                   withDockerRegistry(credentialsId: 'docker', toolName: 'docker'){   
+                       sh "docker push sudhakarp1703/netflix:latest "
+                   }      
+                }               
+            }
+        }  
+         stage ('Build a Container') {
+            steps {
+                sh "docker run -itd --name Netflix -p 8081:80 sudhakarp1703/netflix:latest"
+            }               
+        }
+    }  
+}
+
